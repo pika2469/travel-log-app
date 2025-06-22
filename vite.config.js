78791514b70d3619b57b9e5d85dc7d52724b19f5
pika2469 <shota_5963@yahoo.co.jp>
2025-06-22 18:40:04 +0900

    register.htmlの再ビルド（docsフォルダ内にビルドされるようにvite.config.jsを修正）

--- vite.config.js
+++ vite.config.js
@@ -3,18 +3,19 @@ import {resolve} from 'path';
 
 export default defineConfig({
     base: './', // 相対パスで出力
-    resolve: {
+    build: {
+        outDir: 'docs',
+        // マルチページ対応
+        rollupOptions: {
+            input: {
+                index: resolve(__dirname, 'index.html'),
+                register: resolve(__dirname, 'register.html'),
+            },
+        },
+    },
+        resolve: {
         alias: {
             leaflet: '/node_modules/leaflet/dist/leaflet.js',
         },
     },
-    build: {
-        outDir: 'docs',
-        // マルチページ対応
-        input: {
-            index: resolve(__dirname, 'index.html'),
-            register: resolve(__dirname, 'register.html'),
-        },
-    },
-
 });
